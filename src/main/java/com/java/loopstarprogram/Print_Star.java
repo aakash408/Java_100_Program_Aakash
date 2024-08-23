1a0065b37022a2d05060a129f590e01eb2837206
Aakash Gupta <aakash408408@gmail.com>
2024-08-23 11:14:58 +0530

    Print A to  using Loop

--- src/main/java/com/java/loopstarprogram/Print_Star.java
+++ src/main/java/com/java/loopstarprogram/Print_Star.java
@@ -4,11 +4,11 @@ public class Print_Star {
 
 	public static void main(String[] args) {
 		// Print Star console using Loop
-		
-		int row , numberofstar;
-		
-		for(row=1;row<=10;row++) {
-			for(numberofstar =1;numberofstar<=row;numberofstar++) {
+
+		int row, numberofstar;
+
+		for (row = 1; row <= 10; row++) {
+			for (numberofstar = 1; numberofstar <= row; numberofstar++) {
 				System.out.println("*");
 			}
 			System.out.println(); // Go for the next line
---
+++ src/main/java/com/java/loopstarprogram/alphabet_printusing_loop.java
@@ -0,0 +1,15 @@
+package com.java.loopstarprogram;
+
+public class alphabet_printusing_loop {
+
+	public static void main(String[] args) {
+		// Print all alphabet using for loop Program in Java
+
+		char ch;
+		for (ch = 'a'; ch <= 'z'; ch++) {
+			System.out.println(ch);
+		}
+
+	}
+
+}
