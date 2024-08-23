02fc5cb07c68659ddbe7fc6005295fa758e22f71
Aakash Gupta <aakash408408@gmail.com>
2024-08-23 11:04:43 +0530

    Simple Java Loop Program Added

---
+++ src/main/java/com/java/loopstarprogram/simple_loop.java
@@ -0,0 +1,13 @@
+package com.java.loopstarprogram;
+
+public class simple_loop {
+
+	public static void main(String[] args) {
+		// Simple For Loop Program in Java
+
+		int c;
+		for (c = 1; c < 10; c++) {
+			System.out.println(c);
+		}
+	}
+}
