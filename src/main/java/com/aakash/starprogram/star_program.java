3198171c599e2f8c6bc80bca388be42660475ee5
Aakash Gupta <aakash408408@gmail.com>
2024-08-24 22:54:36 +0530

    Simple loop

---
+++ src/main/java/com/aakash/starprogram/star_program.java
@@ -0,0 +1,15 @@
+package com.aakash.starprogram;
+
+public class star_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a;
+		for (a = 1; a<= 5; a++) {
+			System.out.println(a);
+		}
+
+	}
+
+}
