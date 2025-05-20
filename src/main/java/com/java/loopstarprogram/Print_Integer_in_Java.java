cff36aec3ca05d2f3a1e480f4e46850b021a111d
Aakash Gupta <aakash408408@gmail.com>
2025-05-20 17:01:01 +0530

    Programm Added

---
+++ src/main/java/com/java/loopstarprogram/Print_Integer_in_Java.java
@@ -0,0 +1,15 @@
+package com.java.loopstarprogram;
+
+public class Print_Integer_in_Java {
+
+	public static void main(String[] args) {
+
+		int c; // Declaring the Variable
+
+		/* Using for loop to repeat instruction execution */
+		for (c = 1; c <= 10; c++) {
+			System.out.println(c);
+		}
+
+	}
+}
---
+++ src/main/java/com/java/simple/simple_java_program.java
@@ -0,0 +1,8 @@
+package com.java.simple;
+
+public class simple_java_program {
+	public static void main(String[] args) {
+		
+		System.out.println(" Aakash SDET Engineer " + "and Package is 22 LPA");
+	}
+}
